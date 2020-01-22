9751d73e4096dad33db61fc226e5ae8136928e4f
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-22 16:34:09 +0100

    :art: refactor some code

--- src/Components/UserDetails/UserDetails.js
+++ src/Components/UserDetails/UserDetails.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { UserDetailsContainer } from './UserDetailsContainer';
 import { UserDetailsScreen } from './UserDetailsScreen';
 import { ErrorBoundary } from '../ErrorBoundary';
+import { WrapperCommon } from '../WrapperCommon';
+import { ErrorMessage } from '../ErrorMessage';
 
 export const UserDetails = () => {
     return (
         <React.Suspense fallback={<h3>Loading User Detail...</h3>}>
             <UserDetailsContainer>
-                {({ user, error }) => {
-                    if (error) return <h3>Error Occurs: {error.message}</h3>
-                    return (
-                        <ErrorBoundary centerText>
-                            <UserDetailsScreen user={user} />
-                        </ErrorBoundary>
-                    )
-                }}
+                {({ user, error }) => (
+                    <>
+                        <WrapperCommon isVisible={error}>
+                            <ErrorMessage error={error} />
+                        </WrapperCommon>
+                        <WrapperCommon isVisible={!error}>
+                            <ErrorBoundary centerText>
+                                <UserDetailsScreen user={user} />
+                            </ErrorBoundary>
+                        </WrapperCommon>
+                    </>
+                )}
             </UserDetailsContainer>
         </React.Suspense>
     )
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -6,7 +6,7 @@ export const Users = ({ nameUserList, onClick }) => {
     return (
         <StyledWrapper data-testid="users-block">
             {nameUserList.map((user) => (
-                <ErrorBoundary>
+                <ErrorBoundary key={user.id}>
                     <StyledUserItem key={user.id} data-testid="username-item" onClick={() => onClick(user.id)}>
                         Name: {user.name}
                     </StyledUserItem>
