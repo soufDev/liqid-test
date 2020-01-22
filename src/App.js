327a6dcb2713b7a3a557de029d4f0ba040c03e15
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-22 10:32:34 +0100

    :sparkles: add styled compoent Context for theming

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes } from './Routes';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <h1>LIQID TEST</h1>
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,4 @@
+export default {
+    blue: '#0073b1',
+    white: '#111'
+}
