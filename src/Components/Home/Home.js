70b1679426251b4039451f619535c4dd881c0faf
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-22 12:22:03 +0100

    :sparkles: add api call to Ho,e Component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { Users } from '../Users';
 import { HomeContainer } from './HomeContainer';
 
-const items = [
-    { name: "Josh", id: 1 },
-    { name: "Daniel", id: 2 },
-    { name: "Tom", id: 3 },
-]
 export const Home = () => {
     return (
-        <HomeContainer>
-            {({ users, isLoading, error }) => {
-                if (isLoading) return <h3>Loading ...</h3>;
-                if (error) return <h3>Error Occurred: {error.message}</h3>
-                return <Users nameUserList={items} />
-            }}
-        </HomeContainer>
+        <React.Suspense fallback={<h3>Loading...</h3>}>
+            <HomeContainer>
+                {({ users, error }) => {
+                    if (error) return <h3>Error Occurred: {error.message}</h3>
+                    return <Users nameUserList={users} />
+                }}
+            </HomeContainer>
+        </React.Suspense>
     )
 }
--- src/Components/Home/HomeContainer.js
+++ src/Components/Home/HomeContainer.js
@@ -1,6 +1,7 @@
+import useSWR from "swr";
+import { fetcher } from "../../api";
+
 export const HomeContainer = ({ children }) => {
-    const users = [];
-    const isLoading = false;
-    const error = null;
-    return children({ users, isLoading, error });
+    const { data, error } = useSWR('/users', (path) => fetcher.get(path), { suspense: true });
+    return children({ users: data, error });
 }
