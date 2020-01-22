032abcfd76cf67f883b6a7b24345b01f69de2417
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-22 12:21:36 +0100

    :sparkles: add api call config and swr package to handle Api calls

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const URL = 'https://jsonplaceholder.typicode.com';
+
+class ApiService {
+  client;
+
+  constructor() {
+    const client = axios.create({
+      baseURL: URL,
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    });
+    this.client = client;
+  }
+
+  async get(path, options) {
+    const response = await this.client.get(path, options);
+    return response.data;
+  }
+}
+
+export default new ApiService();
