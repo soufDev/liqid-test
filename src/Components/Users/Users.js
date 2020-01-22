f8461dc30e8235a10897514367c4e774a874a64b
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-22 15:29:32 +0100

    :sparkles: :white_check_mark: add onClick to Users Component and its test

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { StyledWrapper, StyledUserItem } from './Users.styled';
 import { ErrorBoundary } from '../ErrorBoundary';
 
-export const Users = ({ nameUserList }) => {
+export const Users = ({ nameUserList, onClick }) => {
     return (
         <StyledWrapper data-testid="users-block">
             {nameUserList.map((user) => (
                 <ErrorBoundary>
-                    <StyledUserItem key={user.id} data-testid="username-item">
+                    <StyledUserItem key={user.id} data-testid="username-item" onClick={() => onClick(user.id)}>
                         Name: {user.name}
                     </StyledUserItem>
                 </ErrorBoundary>
--- src/Components/Users/Users.test.js
+++ src/Components/Users/Users.test.js
@@ -1,79 +1,89 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Users } from './Users';
 
-test('shoudl render without crashing', () => {
+const mockOnClick = jest.fn((id) => id);
+const items = [
+  {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      }
+  }
+];
+
+test('should render without crashing', () => {
     const { container } = render(<Users nameUserList={[]} />);
     expect(container.firstChild).toBeInTheDocument()
 });
 
 test('should render the right number of username-item', () => {
-    const items = [
-        {
-            "id": 1,
-            "name": "Leanne Graham",
-            "username": "Bret",
-            "email": "Sincere@april.biz",
-            "address": {
-              "street": "Kulas Light",
-              "suite": "Apt. 556",
-              "city": "Gwenborough",
-              "zipcode": "92998-3874",
-              "geo": {
-                "lat": "-37.3159",
-                "lng": "81.1496"
-              }
-            },
-            "phone": "1-770-736-8031 x56442",
-            "website": "hildegard.org",
-            "company": {
-              "name": "Romaguera-Crona",
-              "catchPhrase": "Multi-layered client-server neural-net",
-              "bs": "harness real-time e-markets"
-            }
-          },
-          {
-            "id": 2,
-            "name": "Ervin Howell",
-            "username": "Antonette",
-            "email": "Shanna@melissa.tv",
-            "address": {
-              "street": "Victor Plains",
-              "suite": "Suite 879",
-              "city": "Wisokyburgh",
-              "zipcode": "90566-7771",
-              "geo": {
-                "lat": "-43.9509",
-                "lng": "-34.4618"
-              }
-            },
-            "phone": "010-692-6593 x09125",
-            "website": "anastasia.net",
-            "company": {
-              "name": "Deckow-Crist",
-              "catchPhrase": "Proactive didactic contingency",
-              "bs": "synergize scalable supply-chains"
-            }
-          },
-          {
-            "id": 3,
-            "name": "Clementine Bauch",
-            "username": "Samantha",
-            "email": "Nathan@yesenia.net",
-            "address": {
-              "street": "Douglas Extension",
-              "suite": "Suite 847",
-              "city": "McKenziehaven",
-              "zipcode": "59590-4157",
-              "geo": {
-                "lat": "-68.6102",
-                "lng": "-47.0653"
-              }
-            }
-        }
-    ];
-    const { getAllByTestId, queryAllByTestId, rerender } = render(<Users nameUserList={[]} />);
+    
+    const { getAllByTestId, queryAllByTestId, rerender } = render(<Users nameUserList={[]} onClick={mockOnClick} />);
     expect(queryAllByTestId('username-item').length).toBe(0);
     rerender(<Users nameUserList={items} />);
     expect(getAllByTestId('username-item').length).toBe(items.length); 
+})
+
+test('should call the onClick Method', () => {
+    const { getByText } = render(<Users nameUserList={items} onClick={mockOnClick} />);
+    const item = getByText(/Leanne Graham/);
+    fireEvent.click(item);
+    expect(mockOnClick).toHaveBeenCalled();
 })
