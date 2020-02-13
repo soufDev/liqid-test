21872dc79d8c89d18e4723461157239b5c75185f
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-13 17:21:02 +0100

    :art: add ErrorBoundary to UserInfosContainer

--- src/Components/UserDetails/UserDetailsInfo/index.js
+++ src/Components/UserDetails/UserDetailsInfo/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { UserInfosContainer } from './UserInfosContainer';
-import { WrapperCommon } from '../../WrapperCommon';
-import { ErrorMessage } from '../../ErrorMessage';
-import { ErrorBoundary } from '../../ErrorBoundary';
-import { UserDetailsInfo } from './UserDetailsInfo';
+import React from "react";
+import { UserInfosContainer } from "./UserInfosContainer";
+import { WrapperCommon } from "../../WrapperCommon";
+import { ErrorMessage } from "../../ErrorMessage";
+import { ErrorBoundary } from "../../ErrorBoundary";
+import { UserDetailsInfo } from "./UserDetailsInfo";
 
 export default () => (
-    <React.Suspense fallback={<h4>Loading the user infos...</h4>}>
-        <UserInfosContainer>
-            {({ user, userError }) => (
-                <>
-                    <WrapperCommon isVisible={userError}>
-                        <ErrorMessage error={userError} />
-                    </WrapperCommon>
-                    <WrapperCommon isVisible={!userError}>
-                        <ErrorBoundary>
-                            <UserDetailsInfo user={user} />
-                        </ErrorBoundary>
-                    </WrapperCommon>
-                </>
-            )}
-        </UserInfosContainer>
-    </React.Suspense>
-)
+  <React.Suspense fallback={<h4>Loading the user infos...</h4>}>
+    <ErrorBoundary>
+      <UserInfosContainer>
+        {({ user, userError }) => (
+          <>
+            <WrapperCommon isVisible={userError}>
+              <ErrorMessage error={userError} />
+            </WrapperCommon>
+            <WrapperCommon isVisible={!userError}>
+              <ErrorBoundary>
+                <UserDetailsInfo user={user} />
+              </ErrorBoundary>
+            </WrapperCommon>
+          </>
+        )}
+      </UserInfosContainer>
+    </ErrorBoundary>
+  </React.Suspense>
+);
