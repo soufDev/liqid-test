77e5ae025c03bbd7e9a4bcee98a00947ec0d3666
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-13 17:20:28 +0100

    :art: add ErrorBoundary to userImageContainer

--- src/Components/UserDetails/UserImage/index.js
+++ src/Components/UserDetails/UserImage/index.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { UserImageContainer } from './UserImageContainer';
-import { WrapperCommon } from '../../WrapperCommon';
-import { ErrorMessage } from '../../ErrorMessage';
-import { UserImage } from './UserImage';
+import React from "react";
+import { UserImageContainer } from "./UserImageContainer";
+import { WrapperCommon } from "../../WrapperCommon";
+import { ErrorMessage } from "../../ErrorMessage";
+import { UserImage } from "./UserImage";
+import { ErrorBoundary } from "../../ErrorBoundary";
 
 export default () => (
-    <React.Suspense fallback={<h4>Loading the user image...</h4>}>
-        <UserImageContainer>
-            {({ image, imageError }) => (
-                <>
-                    <WrapperCommon isVisible={imageError}>
-                        <ErrorMessage error={imageError} />
-                    </WrapperCommon>
-                    <WrapperCommon isVisible={!imageError}>
-                        <UserImage url={image.url} title={image.title} />
-                    </WrapperCommon>
-                </>
-            )}
-        </UserImageContainer>
-    </React.Suspense>
-)
+  <React.Suspense fallback={<h4>Loading the user image...</h4>}>
+    <ErrorBoundary>
+      <UserImageContainer>
+        {({ image, imageError }) => (
+          <>
+            <WrapperCommon isVisible={imageError}>
+              <ErrorMessage error={imageError} />
+            </WrapperCommon>
+            <WrapperCommon isVisible={!imageError}>
+              <UserImage url={image.url} title={image.title} />
+            </WrapperCommon>
+          </>
+        )}
+      </UserImageContainer>
+    </ErrorBoundary>
+  </React.Suspense>
+);
